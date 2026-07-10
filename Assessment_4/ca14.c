016c46a7665d2bd824fb07374185cc42ec0ebedc
Harineash T <harineash007@gmail.com>
2026-07-10 15:44:27 +0530

    Update ca14.c

--- Assessment_4/ca14.c
+++ Assessment_4/ca14.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
-#include <math.h>
+
 int main()
 {
-    long int num, temp, first, last;
-    int digits = 0;
+    int number, temp;
+    int firstDigit, lastDigit, middlePart;
+    int power = 1;
+
     printf("Enter a number: ");
-    scanf("%ld", &num);
-    temp = num;
-    last = temp % 10;
+    scanf("%d", &number);
+
+    temp = number;
+
+    // Find the last digit
+    lastDigit = number % 10;
+
+    // Find the first digit and power of 10
     while (temp >= 10)
     {
         temp = temp / 10;
-        digits++;
+        power = power * 10;
     }
-    first = temp;
-    long int middle = num % (long int)pow(10, digits);
-    middle = middle / 10;
-    long int result = last * (long int)pow(10, digits) + middle * 10 + first;
-    printf("Result = %ld", result);
+
+    // First digit
+    firstDigit = temp;
+
+    // Middle part
+    middlePart = (number % power) / 10;
+
+    // Swap first and last digits
+    number = lastDigit * power + middlePart * 10 + firstDigit;
+
+    printf("Number after swapping = %d", number);
+
     return 0;
 }
