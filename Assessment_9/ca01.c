8f821d3dbeebd5c03546bc54e88cf0c20e11e24b
Harineash T <harineash007@gmail.com>
2026-07-29 20:18:38 +0530

    Update ca01.c

--- Assessment_9/ca01.c
+++ Assessment_9/ca01.c
@@ -6,11 +6,11 @@
 int main(){
     
     int a[5],i,sum=0;
-    for (i=1;i<=5;i++)
+    for (i=0;i<5;i++)
     {
         scanf("%d",&a[i]);
     }
-    for (i=1;i<=5;i++)
+    for (i=0;i<5;i++)
     {
         sum+=a[i];
     }
