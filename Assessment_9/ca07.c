c6d12a42a30ccdd7bb8822eff3d54f9e9be1123e
Harineash T <harineash007@gmail.com>
2026-07-29 20:59:45 +0530

    Refactor code for sorting numbers in descending order

--- Assessment_9/ca07.c
+++ Assessment_9/ca07.c
@@ -1,35 +1,30 @@
 // Program to get 5 numbers from user, arrange them in descending order and print the same
 #include <stdio.h>
 
-int main()
-{
-    int a[5], i, j, temp;
-
-    // Read 5 numbers
-    for (i = 0; i < 5; i++)
+int main(){
+    
+    int a[5],i,j,temp;
+    for (i=0;i<5;i++)
     {
-        scanf("%d", &a[i]);
+        scanf("%d",&a[i]);
     }
-
-    // Arrange in descending order
-    for (i = 0; i < 4; i++)
+    
+    for (i=0;i<4;i++)
     {
-        for (j = i + 1; j < 5; j++)
-        {
-            if (a[i] < a[j])
+        for (j=i+1;j<5;j++){
+            if (a[i]<a[j])
             {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
             }
+            
         }
     }
-
-    // Print sorted numbers
-    for (i = 0; i < 5; i++)
+    
+    for (i=0;i<5;i++)
     {
-        printf("%d ", a[i]);
+        printf("%d",a[i]);
     }
-
     return 0;
 }
