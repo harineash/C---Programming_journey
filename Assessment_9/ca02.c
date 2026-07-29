948dc3bc4b7bfe43d3eebb6f9eafcf35eeb5c410
Harineash T <harineash007@gmail.com>
2026-07-29 20:19:08 +0530

    Update ca02.c

--- Assessment_9/ca02.c
+++ Assessment_9/ca02.c
@@ -1,15 +1,14 @@
 // Program to get 5 numbers from user and print the avg of all numbers using array and for loop
 #include <stdio.h>
-#include <stdio.h>
 
 int main(){
     
     int a[5],i,sum=0,avg;
-    for (i=1;i<=5;i++)
+    for (i=1;i<5;i++)
     {
         scanf("%d",&a[i]);
     }
-    for (i=1;i<=5;i++)
+    for (i=0;i<5;i++)
     {
         sum+=a[i];
     }
