e2de79a35bd519563b3a9acbe23a825f425e77f5
Harineash T <harineash007@gmail.com>
2026-09-20 09:29:15 +0530

    Add files via upload

--- Assessment_4/ca13.c
+++ Assessment_4/ca13.c
@@ -1,15 +1,32 @@
+// Reversing the number
 #include <stdio.h>
 int main()
 {
-    long int num, rev = 0, digit;
+    int n, rev = 0, digit;
+
     printf("Enter a number: ");
-    scanf("%ld", &num);
-    while (num != 0)
+    scanf("%d", &n);
+
+    if (n == 0)
     {
-        digit = num % 10;       // get last digit
-        rev = rev * 10 + digit; // build reverse
-        num = num / 10;         // remove last digit
+        rev = 0;
+        goto end;
     }
-    printf("Reversed number = %ld", rev);
+
+start:
+    if (n != 0)
+    {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+        goto start;
+    }
+    else
+    {
+        goto end;
+    }
+
+end:
+    printf("Reverse = %d", rev);
     return 0;
 }
