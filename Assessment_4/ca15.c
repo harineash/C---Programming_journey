b79a67bdcfed629c8a9be8c79a69a7780e60b975
Harineash T <harineash007@gmail.com>
2026-09-20 09:29:43 +0530

    Add files via upload

--- Assessment_4/ca15.c
+++ Assessment_4/ca15.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
+
 int main()
 {
-    long int num;
-    int last;
-    scanf("%ld", &num);
-    last = num % 10;
-    if (last % 2 != 0)
-        num = num - 1;
-    printf("%ld", num);
+    int n, temp, first, digits = 0, pow10 = 1, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+count:
+    if (temp != 0)
+    {
+        digits++;
+        temp = temp / 10;
+        goto count;
+    }
+
+power:
+    if (digits > 1)
+    {
+        pow10 = pow10 * 10;
+        digits--;
+        goto power;
+    }
+
+    first = n / pow10;
+
+    if (first % 2 != 0)
+    {
+        first = first - 1;
+    }
+
+    result = first * pow10 + (n % pow10);
+
+    printf("%d", result);
     return 0;
 }
