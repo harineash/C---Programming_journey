adf7eb185cd6116f779d7d8b79851189173d34e6
Harineash T <harineash007@gmail.com>
2026-09-20 09:26:48 +0530

    Add files via upload

--- Assessment_4/ca09.c
+++ Assessment_4/ca09.c
@@ -1,17 +1,30 @@
+// Printing even numbers with sum 6
 #include <stdio.h>
 int main()
 {
-    int i, sum, a, b;
+    int i = 10, a, b;
 
-    for (i = 10; i <= 99; i++)
+start:
+    if (i <= 99)
     {
-        a = i / 10;
-        b = i % 10;
-        sum = a + b;
-        if (i % 2 == 0 && sum == 6)
+        if (i % 2 == 0)
         {
-            printf("%d\n", i);
+            a = i / 10;
+            b = i % 10;
+
+            if (a + b == 6)
+            {
+                printf("%d\n", i);
+            }
         }
+        i++;
+        goto start;
     }
+    else
+    {
+        goto end;
+    }
+
+end:
     return 0;
 }
